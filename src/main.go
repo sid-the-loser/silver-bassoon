6d18334a45d8a4bb88a690368c907f692a76ade3
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-03-26 04:44:48 -0400

    added argument parsing no file generation yet

--- src/main.go
+++ src/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
-	args := os.Args[1:]
+	winWidth := flag.Int("width", 800, "The width of the Pygame window")
+	winHeight := flag.Int("height", 600, "The height of the Pygame window")
 
-	fmt.Println("Arguments", args)
+	flag.Parse()
+
+	filename := flag.Arg(0)
+
+	formattedString := fmt.Sprintf("%s %d %d", filename, *winWidth, *winHeight)
+
+	fmt.Printf(formattedString)
 }
