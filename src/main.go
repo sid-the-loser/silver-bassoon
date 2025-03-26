71181c0182f027d51d05763ef240531249572d63
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-03-26 18:12:16 -0400

    added two window flags resizable and scalable (two of the tags I use the most)

--- src/main.go
+++ src/main.go
@@ -11,6 +11,9 @@ func main() {
 	winWidth := flag.Int("width", 800, "The width of the Pygame window")
 	winHeight := flag.Int("height", 600, "The height of the Pygame window")
 
+	useScaling := flag.Bool("scalable", false, "Makes Pygame window scalable")
+	useResize := flag.Bool("resizable", false, "Makes Pygame window resizable")
+
 	flag.Parse()
 
 	filename := flag.Arg(0)
