d1f054c6d9d68540a51a8114550ecca07232a1f1
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-03-26 18:23:39 -0400

    added error message for filename incase you're wondering why half of my code is commented, its because go do be like that

--- src/main.go
+++ src/main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 )
 
 var version = "1.0.0"
 
 func main() {
-	winWidth := flag.Int("width", 800, "The width of the Pygame window")
-	winHeight := flag.Int("height", 600, "The height of the Pygame window")
+	// winWidth := flag.Int("width", 800, "The width of the Pygame window")
+	// winHeight := flag.Int("height", 600, "The height of the Pygame window")
 
-	useScaling := flag.Bool("scalable", false, "Makes Pygame window scalable")
-	useResize := flag.Bool("resizable", false, "Makes Pygame window resizable")
+	// useScaling := flag.Bool("scalable", false, "Makes Pygame window scalable")
+	// useResize := flag.Bool("resizable", false, "Makes Pygame window resizable")
 
 	flag.Parse()
 
 	filename := flag.Arg(0)
 
-	formattedString := fmt.Sprintf("%s %d %d", filename, *winWidth, *winHeight)
-
-	fmt.Printf(formattedString) // just printing the formatted text for now,
-	// will work on the rest of the logic later
+	if len(filename) == 0 {
+		log.Fatalln("No filename provided to generate the Pygame script!")
+	}
 }
