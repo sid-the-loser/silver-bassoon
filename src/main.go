0463a124f691a63948cad9a36fcf120897e2d410
sid-the-loser <sidharthsuresh07@gmail.com>
2025-04-13 13:56:08 -0400

    file existing checks
    - more comments added
    - file existing checks taken from stackoverflow

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
 const version = "pr1.0.0"
@@ -18,7 +19,7 @@ func main() {
 	//useScaling := flag.Bool("scalable", false, "Makes Pygame window scalable")
 	//useResize := flag.Bool("resizable", false, "Makes Pygame window resizable")
 
-	flag.Parse()
+	flag.Parse() // Parse all the above defined flags
 
 	// checks if the version flag is enabled, if so, show version and stop program
 	// todo: try adding GitHub API based version checkups
@@ -29,7 +30,14 @@ func main() {
 
 	filename := flag.Arg(0)
 
-	if len(filename) == 0 {
+	if len(filename) == 0 { // no filename used
 		log.Fatalln("No filename provided to generate the Pygame script! " + commonErrorSuffix)
+	} else if _, err := os.Stat("sample.txt"); err == nil { // if file exists
+		// does nothing and continues with the rest of the program
+		// this part off the code is taken from:
+		// https://stackoverflow.com/questions/12518876/how-to-check-if-a-file-exists-in-go
+	} else {
+		log.Fatalln("The filename you've passed doesn't exist!") // if file doesn't exist
 	}
+
 }
